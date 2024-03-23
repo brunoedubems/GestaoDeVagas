a25b0c043e9a2e98fdc548eaf5be7284da099343
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2024-03-23 15:56:48 -0300

    Gestao de vagas projeto

---
+++ src/main/java/br/com/brunoedubems4/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,21 @@
+package br.com.brunoedubems4.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+
+    
+
+
+}
