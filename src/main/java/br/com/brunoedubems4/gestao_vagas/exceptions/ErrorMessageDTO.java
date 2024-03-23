a16b755d93cb5878d185b2f2c74fdee9bca22b21
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2024-03-23 18:25:26 -0300

    99

---
+++ src/main/java/br/com/brunoedubems4/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package br.com.brunoedubems4.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    
+    private String message;
+    private String field;
+}
--- src/main/java/br/com/brunoedubems4/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/brunoedubems4/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,11 @@ package br.com.brunoedubems4.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,13 +14,19 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços")
     private String username;
+
+    @Email(message = "O campo [email] deve conter  um e-mail válido")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
+
     private String description;
     private String curriculum;
-
-    
 
 
 }
