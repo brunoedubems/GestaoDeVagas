a2fef79273e0be3cefa0457087b7e71378070c4b
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2024-03-27 15:34:59 -0300

    docker

--- src/main/java/br/com/brunoedubems4/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/brunoedubems4/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,18 +1,29 @@
 package br.com.brunoedubems4.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name="candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    // @Column(name="nome")
     private String name;
 
     @NotBlank
@@ -28,5 +39,7 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
