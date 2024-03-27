0a9dbccea6b5447ecf372e79a58ed3cd33bdde4b
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2024-03-27 15:35:13 -0300

    docker2

--- src/main/java/br/com/brunoedubems4/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/brunoedubems4/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,11 +1,13 @@
 package br.com.brunoedubems4.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.brunoedubems4.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.brunoedubems4.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
@@ -13,12 +15,13 @@ import jakarta.validation.Valid;
 
 public class CandidateController {
     
+    @Autowired
+    private CandidateRepository candidaterespository;
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity){
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+        return this.candidaterespository.save(candidateEntity);
 
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
     }
  
 }
